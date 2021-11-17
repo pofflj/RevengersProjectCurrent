62b20ddbe352315ad682ee53b02724f5d20c8ad6
pofflj <Pofflj@etsu.edu>
2021-11-17 15:14:15 -0500

    Background Image changes
    Background Images in the resource file

--- RevengerProject3/RevengerProject3/PlayerCardForAll.Designer.cs
+++ RevengerProject3/RevengerProject3/PlayerCardForAll.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayerCardForAll));
             this.p1Name = new System.Windows.Forms.Label();
             this.p2Name = new System.Windows.Forms.Label();
             this.p3Name = new System.Windows.Forms.Label();
@@ -347,6 +348,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.hideButton);
             this.Controls.Add(this.p2class);
