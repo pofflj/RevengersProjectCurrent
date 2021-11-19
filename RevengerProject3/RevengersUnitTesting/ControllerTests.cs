ba470600960ceaed5501b0c3443c3770453c0d0b
MackinleyReece <89943348+MackinleyReece@users.noreply.github.com>
2021-11-19 17:41:16 -0500

    Unit Testing Update

--- RevengerProject3/RevengersUnitTesting/ControllerTests.cs
+++ RevengerProject3/RevengersUnitTesting/ControllerTests.cs
@@ -28,5 +28,22 @@ namespace RevengerProject3.Tests
             var result = _controller.ResearchResource(_player);
             Assert.IsTrue(result>0, "Number greater than 0");
         }
+
+        [Test()]
+        public void ResearchResourceTest_InputIsNegative()
+        {
+            _player.WorkersAtResearchLab = 1;
+            var result = _controller.ResearchResource(_player);
+            Assert.IsFalse(result < 0, "Number less than 0");
+        }
+
+        // Test for failure
+        [Test()]
+        public void ResearchResourceTest_InputToTestFailure()
+        {
+            _player.WorkersAtResearchLab = 0;
+            var result = _controller.ResearchResource(_player);
+            Assert.IsTrue(result > 0, "Test is a failure");
+        }
     }
 }
